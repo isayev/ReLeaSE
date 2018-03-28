ae1081ed99c98d6d44f2ec7e291115e14404c50e
mariewelt <mariewelt@gmail.com>
2018-03-28 17:57:44 -0400

    Policy Gradient and Policy Gradient with Replay Memory

--- reinforcement.py
+++ reinforcement.py
@@ -17,7 +17,7 @@ class Reinforcement(object):
         reward = 0
         n_samples = 0
 
-        for i in range(n_batch):
+        for _ in range(n_batch):
 
             hidden = self.generator.init_hidden()
             cell = self.generator.init_cell()
@@ -43,3 +43,58 @@ class Reinforcement(object):
         self.generator.optimizer.step()
 
         return rl_loss.data[0]
+
+    def policy_gradient(self, prime_str='<', end_token='>', predict_len=200, temperature=0.8, n_batch=100):
+
+        rl_loss = 0
+        reward = 0
+        n_samples = 0
+
+        for _ in range(n_batch):
+
+            hidden = self.generator.init_hidden()
+            cell = self.generator.init_cell()
+            stack = self.generator.initStack()
+            prime_input = self.data.char_tensor(prime_str)
+            predicted = prime_str
+
+            # Use priming string to "build up" hidden state
+            for p in range(len(prime_str)):
+                _, hidden, cell, stack = self.generator(prime_input[p], hidden, cell, stack)
+            inp = prime_input[-1]
+
+            cur_loss = 0
+            for p in range(predict_len):
+                output, hidden, cell, stack = self.generator(inp, hidden, cell, stack)
+
+                # Sample from the network as a multinomial distribution
+                output_dist = output.data.view(-1).div(temperature).exp()
+                top_i = torch.multinomial(output_dist, 1)[0]
+                log_dist = F.log_softmax(output)
+                cur_loss += log_dist[0, top_i]
+
+                # Add predicted character to string and use as next input
+                predicted_char = self.data.all_characters[top_i]
+                predicted += predicted_char
+                inp = char_tensor(predicted_char)
+
+                if predicted_char == end_token:
+                    reward = self.get_reward(predicted[1:-1])
+                else:
+                    reward = -10
+
+            if reward != 0.0:
+                rl_loss += cur_loss * reward
+                n_samples += 1
+
+        rl_loss = -rl_loss / n_samples
+        rl_loss.backward()
+        self.generator.optimizer.step()
+
+        return rl_loss.data[0]
+
+    def transfer_learning(self):
+        raise NotImplementedError
+
+
+
