955789c3095ad63b1496e13bb5b1ea29fba8d03f
mariewelt <mariewelt@gmail.com>
2018-03-29 16:42:45 -0400

    Added 'get_reward' and 'transfer_learning' functions

--- reinforcement.py
+++ reinforcement.py
@@ -1,17 +1,26 @@
+import torch
 import torch.nn.functional as F
 
 
 class Reinforcement(object):
-    def __init__(self, generator, data):
+    def __init__(self, generator, predictor):
         super(Reinforcement, self).__init__()
         self.generator = generator
-        self.data = data
+        self.predictor = predictor
         raise NotImplementedError
 
-    def get_reward(self, smiles):
-        raise NotImplementedError
+    def get_reward(self, smiles, threshold, invalid_reward=-2.0):
+        # Add continuous reward
+        tmp, nan_smiles = self.predictor.predict([smiles])
+        if len(nan_smiles) == 1:
+            return invalid_reward
+        prop = tmp[0, 1]
+        if prop >= threshold:
+            return 1.0
+        else:
+            return -1.0
 
-    def policy_gradient_replay(self, n_batch=100):
+    def policy_gradient_replay(self, data, replay_memory, n_batch=100):
 
         rl_loss = 0
         reward = 0
@@ -23,8 +32,8 @@ class Reinforcement(object):
             cell = self.generator.init_cell()
             stack = self.generator.initStack()
 
-            seq = self.data.sample_from_replay_memory()
-            inp = self.data.char_tensor(seq)
+            seq = replay_memory.sample()
+            inp = data.char_tensor(seq)
             cur_loss = 0
 
             for p in range(len(inp)-1):
@@ -44,7 +53,7 @@ class Reinforcement(object):
 
         return rl_loss.data[0]
 
-    def policy_gradient(self, prime_str='<', end_token='>', predict_len=200, temperature=0.8, n_batch=100):
+    def policy_gradient(self, data, prime_str='<', end_token='>', predict_len=200, temperature=0.8, n_batch=100):
 
         rl_loss = 0
         reward = 0
@@ -55,7 +64,7 @@ class Reinforcement(object):
             hidden = self.generator.init_hidden()
             cell = self.generator.init_cell()
             stack = self.generator.initStack()
-            prime_input = self.data.char_tensor(prime_str)
+            prime_input = data.char_tensor(prime_str)
             predicted = prime_str
 
             # Use priming string to "build up" hidden state
@@ -74,9 +83,9 @@ class Reinforcement(object):
                 cur_loss += log_dist[0, top_i]
 
                 # Add predicted character to string and use as next input
-                predicted_char = self.data.all_characters[top_i]
+                predicted_char = data.all_characters[top_i]
                 predicted += predicted_char
-                inp = char_tensor(predicted_char)
+                inp = data.char_tensor(predicted_char)
 
                 if predicted_char == end_token:
                     reward = self.get_reward(predicted[1:-1])
@@ -93,8 +102,6 @@ class Reinforcement(object):
 
         return rl_loss.data[0]
 
-    def transfer_learning(self):
-        raise NotImplementedError
-
-
+    def transfer_learning(self, data, n_epochs):
+        _ = self.generator(data, n_epochs)
 
