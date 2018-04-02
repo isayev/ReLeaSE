648ae3c3a3309941fda2e43a55dabe9400527b10
mariewelt <mariewelt@gmail.com>
2018-04-02 17:52:03 -0400

    hashing function added

--- zinc.py
+++ zinc.py
@@ -1,33 +1,29 @@
-import sys
 from rdkit import Chem
-import numpy as np
-import csv
 import pickle
-import glob, os
 
-directories_1 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K']
-dirs = []
-for d1 in directories_1:
-    for d2 in directories_1:
-        dirs.append(d1 + d2)
 
-for d in dirs[:40]:
-    zinc_inchi = []
-    zinc_ids = []
+def hashing(list_of_files, where_to_write):
 
-    for file in glob.glob('/data/mariewelt/ZINC15/' + d + '/*.smi'):
-        reader = csv.reader(open(file, 'r'), delimiter=' ')
-        data_full = np.array(list(reader))
-        if len(data_full) > 0:
-            smiles = data_full[1:, 0]
-            ids = data_full[1:, 1]
-            zinc_inchi = zinc_inchi + [Chem.InchiToInchiKey(Chem.MolToInchi(Chem.MolFromSmiles(sm)))[:14] for sm in
-                                       smiles]
-            zinc_ids = zinc_ids + [int(x[4:]) for x in ids]
+    counter = 0
+    my_smiles = []
+    for filename in list_of_files:
+        f = open(filename, 'r')
+        for line in f:
+            my_smiles.append(line[:-1])
+    f.close()
 
-    zinc_inchi_hash = [hash(key) for key in zinc_inchi]
-    assert len(zinc_inchi_hash) == len(zinc_ids)
-    dic = {'zinc_ids': zinc_ids, 'inchi_hash': zinc_inchi_hash}
-    pickle.dump(dic, open('/home/mariewelt/Notebooks/PyTorch/data/ZINC15_/' + d + '.pkl', 'wb'))
+    inchi_keys = []
+    new_smiles = []
+    for sm in my_smiles:
+        try:
+            inchi_keys.append(Chem.InchiToInchiKey(Chem.MolToInchi(Chem.MolFromSmiles(sm)))[:14])
+            new_smiles.append(sm)
+        except:
+            raise UserWarning('SMILES string ' + sm + ' can not be hashed')
 
-    print('Directory ' + d + ' is processed!')
+    inchi_hash = [hash(key) for key in inchi_keys]
+
+    dic = {'smiles': new_smiles, 'inchi_hash': inchi_hash}
+    pickle.dump(dic, open(where_to_write + str(counter) + '.pkl', 'wb'))
+
+    return f.closed()
