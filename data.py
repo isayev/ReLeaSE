f6a9a404def8d779450bd2579e37e051d204f0cb
mariewelt <mariewelt@gmail.com>
2018-04-02 17:17:50 -0400

    Data class separated into GeneratorData and PredictorData classes; static methods converted into functions

--- data.py
+++ data.py
@@ -11,16 +11,16 @@ from rdkit import Chem
 from rdkit import DataStructs
 
 
-class Data(object):
+class GeneratorData(object):
     def __init__(self, training_data_path, replay_data=None, replay_capacity=10000, use_cuda=None):
-        super(Data, self).__init__()
-        self.file, success = self.read_smi_file(training_data_path, unique=True)
+        super(GeneratorData, self).__init__()
+        self.file, success = read_smi_file(training_data_path, unique=True)
 
         assert success
         self.file_len = len(self.file)
-        self.all_characters, self.char2idx, self.n_characters = self.tokenize(self.file)
+        self.all_characters, self.char2idx, self.n_characters = tokenize(self.file)
         if replay_data is not None:
-            self.replay_memory, success = self.read_smi_file(replay_data)
+            self.replay_memory, success = read_smi_file(replay_data)
             assert success
         else:
             self.replay_memory = []
@@ -36,7 +36,7 @@ class Data(object):
             smiles (list): SMILES to be added into replay memory.
         If replay_memory exceeds capacity, old examples are pop out from queue.
         """
-        new_smiles = self.canonize_smiles(smiles)
+        new_smiles = canonize_smiles(smiles)
         new_smiles = list(set(new_smiles))
         for sm in new_smiles:
             if not np.isnan(sm):
@@ -88,201 +88,217 @@ class Data(object):
     def read_sdf_file(self, path, fields_to_read):
         raise NotImplementedError
 
-    @staticmethod
-    def read_smiles_property_file(path, delimiter=',', keep_header=False ):
-        reader = csv.reader(open(path, 'r'), delimiter=delimiter)
-        data_full = np.array(list(reader))
-        if keep_header:
-            start_position = 0
-        else:
-            start_position = 1
-        assert len(data_full) > start_position
-        smiles = data_full[start_position:, 1]
-        labels = np.array(data_full[start_position:, 2], dtype='float')
-        assert len(smiles) == len(labels)
-        return smiles, labels
 
-    @staticmethod
-    def cross_validation_split(data, labels, n_folds=5, split='random', folds=None):
-        if split not in ['random', 'fixed']:
-            raise ValueError('Invalid value for argument \'split\': must be either \'random\' of \'fixed\'')
-        n = len(data)
-        assert n > 0
-        if split == 'fixed' and folds is None:
-            raise TypeError('Invalid type for argument \'folds\': found None, but must be list')
-        if split == 'random' and folds is not None:
-            raise UserWarning('\'folds\' argument will be ignored: \'split\' set to random, '
-                              'but \'folds\' argument is provided.')
+class PredictorData(object):
+    def __init__(self, path, delimiter=',', use_cuda=None):
+        super(PredictorData, self).__init__()
+        self.smiles, self.property = read_smiles_property_file(path, delimiter=delimiter)
 
-        if split == 'random':
-            fold_len = round(n / n_folds)
-            folds = []
-            for i in range(n_folds):
-                folds = folds + [i]*fold_len
-            if len(folds) > n:
-                folds = folds[:n]
-            if len(folds) < n:
-                folds = folds + [i]*(n - len(folds))
-            assert(len(folds) == n)
-            ind = np.random.permutation(n)
-            new_data = []
-            new_labels = []
-            for i in ind:
-                new_data.append(data[i])
-                new_labels.append(labels[i])
-            data = new_data
-            labels = new_labels
+        assert len(self.smiles) == len(self.property)
+        self.all_characters, self.char2idx, self.n_characters = self.tokenize(self.smiles)
+        self.use_cuda = use_cuda
+        if self.use_cuda is None:
+            self.use_cuda = torch.cuda.is_available()
+        self.binary_labels = None
 
-        cross_val_data = []
-        cross_val_labels = []
-        for f in range(n_folds):
-            left = np.where(folds == f)[0].min()
-            right = np.where(folds == f)[0].max()
-            cross_val_data.append(data[left:right + 1])
-            cross_val_labels.append(list(labels[left:right + 1]))
+    def binarize(self, threshold):
+        self.binary_labels = np.array(self.property >= threshold, dtype='int32')
 
-        return cross_val_data, cross_val_labels
 
-    @staticmethod
-    def time_since(since):
-        s = time.time() - since
-        m = math.floor(s / 60)
-        s -= m * 60
-        return '%dm %ds' % (m, s)
+def get_fp(self, smiles):
+    fp = []
+    for mol in smiles:
+        fp.append(mol2image(mol, n=2048))
+    return fp
 
-    @staticmethod
-    def tokenize(smiles):
-        """
-        Returns list of unique tokens, token-2-index dictionary and number of unique tokens from the list of SMILES
 
+def mol2image(x: str, n: int = 2048) -> object:
+    try:
+        m = Chem.MolFromSmiles(x)
+        fp = Chem.RDKFingerprint(m, maxPath=4, fpSize=n)
+        res = np.zeros(len(fp))
+        DataStructs.ConvertToNumpyArray(fp, res)
+        return res
+    except UserWarning('Unable to calculate Fingerprint'):
+        return [np.nan]
+
+
+def sanitize_smiles(smiles, canonize=True):
+    """
+    Takes list of SMILES strings and returns list of their sanitized versions.
+    For definition of sanitized SMILES check http://www.rdkit.org/docs/api/rdkit.Chem.rdmolops-module.html#SanitizeMol
         Args:
-            smiles (list): list of SMILES strings to tokenize.
+            smiles (list): list of SMILES strings
+            canonize (bool): parameter specifying whether to return canonical SMILES or not.
 
-        Returns:
-            tokens (list): list of unique tokens/SMILES alphabet.
-            token2idx (dict): dictionary mapping token to its index.
-            num_tokens (int): number of unique tokens.
-        """
-        tokens = list(set(''.join(smiles)))
-        tokens = ''.join(tokens)
-        token2idx = dict((token, i) for i, token in enumerate(tokens))
-        num_tokens = len(tokens)
-        return tokens, token2idx, num_tokens
+        Output:
+            new_smiles (list): list of SMILES and NaNs if SMILES string is invalid or unsanitized.
+            If 'canonize = True', return list of canonical SMILES.
 
-    @staticmethod
-    def read_smi_file(filename: str, unique: bool = True, ) -> tuple:
-        """
-        Reads SMILES from file. File must contain one SMILES string per line
-        with \n token in the end of the line.
+        When 'canonize = True' the function is analogous to: canonize_smiles(smiles, sanitize=True).
+    """
+    new_smiles = []
+    for sm in smiles:
+        try:
+            if canonize:
+                new_smiles.append(Chem.MolToSmiles(Chem.MolFromSmiles(sm, sanitize=True)))
+            else:
+                new_smiles.append(sm)
+        except UserWarning('Unsanitized SMILES string: ' + sm):
+            new_smiles.append(np.nan)
+    return new_smiles
+
+
+def canonize_smiles(smiles, sanitize=True):
+    """
+    Takes list of SMILES strings and returns list of their canonical SMILES.
+        Args:
+            smiles (list): list of SMILES strings
+            sanitize (bool): parameter specifying whether to sanitize SMILES or not.
+            For definition of sanitized SMILES check
+            http://www.rdkit.org/docs/api/rdkit.Chem.rdmolops-module.html#SanitizeMol
+
+        Output:
+            new_smiles (list): list of canonical SMILES and NaNs if SMILES string is invalid or unsanitized
+            (when 'sanitize = True')
+
+        When 'sanitize = True' the function is analogous to: sanitize_smiles(smiles, canonize=True).
+    """
+    new_smiles = []
+    for sm in smiles:
+        try:
+            new_smiles.append(Chem.MolToSmiles(Chem.MolFromSmiles(sm, sanitize=sanitize)))
+        except UserWarning(sm + ' can not be canonized: invalid SMILES string!'):
+            new_smiles.append(np.nan)
+    return new_smiles
+
+
+def save_smi_to_file(filename, smiles, unique=True):
+    """
+    Takes path to file and list of SMILES strings and writes SMILES to the specified file.
 
         Args:
             filename (str): path to the file
-            unique (bool): return only unique SMILES
+            smiles (list): list of SMILES strings
+            unique (bool): parameter specifying whether to write only unique copies or not.
 
-        Returns:
-            smiles (list): list of SMILES strings from specified file.
+        Output:
             success (bool): defines whether operation was successfully completed or not.
+       """
+    if unique:
+        smiles = list(set(smiles))
+    else:
+        smiles = list(smiles)
+    f = open(filename, 'w')
+    for mol in smiles:
+        f.writelines([mol, '\n'])
+    f.close()
+    return f.closed
 
-        If 'unique=True' this list contains only unique copies.
-        """
-        f = open(filename, 'r')
-        molecules = []
-        for line in f:
-            molecules.append(line[:-1])
-        if unique:
-            molecules = list(set(molecules))
-        else:
-            molecules = list(molecules)
-        f.close()
-        return molecules, f.closed
 
-    @staticmethod
-    def save_smi_to_file(filename, smiles, unique=True):
-        """
-        Takes path to file and list of SMILES strings and writes SMILES to the specified file.
+def read_smi_file(filename: str, unique: bool = True, ) -> tuple:
+    """
+    Reads SMILES from file. File must contain one SMILES string per line
+    with \n token in the end of the line.
 
-            Args:
-                filename (str): path to the file
-                smiles (list): list of SMILES strings
-                unique (bool): parameter specifying whether to write only unique copies or not.
+    Args:
+        filename (str): path to the file
+        unique (bool): return only unique SMILES
 
-            Output:
-                success (bool): defines whether operation was successfully completed or not.
-           """
-        if unique:
-            smiles = list(set(smiles))
-        else:
-            smiles = list(smiles)
-        f = open(filename, 'w')
-        for mol in smiles:
-            f.writelines([mol, '\n'])
-        f.close()
-        return f.closed
+    Returns:
+        smiles (list): list of SMILES strings from specified file.
+        success (bool): defines whether operation was successfully completed or not.
 
-    @staticmethod
-    def canonize_smiles(smiles, sanitize=True):
-        """
-        Takes list of SMILES strings and returns list of their canonical SMILES.
-            Args:
-                smiles (list): list of SMILES strings
-                sanitize (bool): parameter specifying whether to sanitize SMILES or not.
-                For definition of sanitized SMILES check
-                http://www.rdkit.org/docs/api/rdkit.Chem.rdmolops-module.html#SanitizeMol
+    If 'unique=True' this list contains only unique copies.
+    """
+    f = open(filename, 'r')
+    molecules = []
+    for line in f:
+        molecules.append(line[:-1])
+    if unique:
+        molecules = list(set(molecules))
+    else:
+        molecules = list(molecules)
+    f.close()
+    return molecules, f.closed
 
-            Output:
-                new_smiles (list): list of canonical SMILES and NaNs if SMILES string is invalid or unsanitized
-                (when 'sanitize = True')
 
-            When 'sanitize = True' the function is analogous to: sanitize_smiles(smiles, canonize=True).
-        """
-        new_smiles = []
-        for sm in smiles:
-            try:
-                new_smiles.append(Chem.MolToSmiles(Chem.MolFromSmiles(sm, sanitize=sanitize)))
-            except UserWarning(sm + ' can not be canonized: invalid SMILES string!'):
-                new_smiles.append(np.nan)
-        return new_smiles
+def tokenize(smiles):
+    """
+    Returns list of unique tokens, token-2-index dictionary and number of unique tokens from the list of SMILES
 
-    @staticmethod
-    def sanitize_smiles(smiles, canonize=True):
-        """
-        Takes list of SMILES strings and returns list of their sanitized versions.
-        For definition of sanitized SMILES check http://www.rdkit.org/docs/api/rdkit.Chem.rdmolops-module.html#SanitizeMol
-            Args:
-                smiles (list): list of SMILES strings
-                canonize (bool): parameter specifying whether to return canonical SMILES or not.
+    Args:
+        smiles (list): list of SMILES strings to tokenize.
 
-            Output:
-                new_smiles (list): list of SMILES and NaNs if SMILES string is invalid or unsanitized.
-                If 'canonize = True', return list of canonical SMILES.
+    Returns:
+        tokens (list): list of unique tokens/SMILES alphabet.
+        token2idx (dict): dictionary mapping token to its index.
+        num_tokens (int): number of unique tokens.
+    """
+    tokens = list(set(''.join(smiles)))
+    tokens = ''.join(tokens)
+    token2idx = dict((token, i) for i, token in enumerate(tokens))
+    num_tokens = len(tokens)
+    return tokens, token2idx, num_tokens
 
-            When 'canonize = True' the function is analogous to: canonize_smiles(smiles, sanitize=True).
-        """
-        new_smiles = []
-        for sm in smiles:
-            try:
-                if canonize:
-                    new_smiles.append(Chem.MolToSmiles(Chem.MolFromSmiles(sm, sanitize=True)))
-                else:
-                    new_smiles.append(sm)
-            except UserWarning('Unsanitized SMILES string: ' + sm):
-                new_smiles.append(np.nan)
-        return new_smiles
 
-    @staticmethod
-    def mol2image(x: str, n: int = 2048) -> object:
-        try:
-            m = Chem.MolFromSmiles(x)
-            fp = Chem.RDKFingerprint(m, maxPath=4, fpSize=n)
-            res = np.zeros(len(fp))
-            DataStructs.ConvertToNumpyArray(fp, res)
-            return res
-        except UserWarning('Unable to calculate Fingerprint'):
-            return [np.nan]
+def time_since(since):
+    s = time.time() - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
 
-    @staticmethod
-    def get_fp(self, smiles):
-        fp = []
-        for mol in smiles:
-            fp.append(self.mol2image(mol, n=2048))
-        return fp
+
+def cross_validation_split(data, labels, n_folds=5, split='random', folds=None):
+    if split not in ['random', 'fixed']:
+        raise ValueError('Invalid value for argument \'split\': must be either \'random\' of \'fixed\'')
+    n = len(data)
+    assert n > 0
+    if split == 'fixed' and folds is None:
+        raise TypeError('Invalid type for argument \'folds\': found None, but must be list')
+    if split == 'random' and folds is not None:
+        raise UserWarning('\'folds\' argument will be ignored: \'split\' set to random, '
+                          'but \'folds\' argument is provided.')
+
+    if split == 'random':
+        fold_len = round(n / n_folds)
+        folds = []
+        for i in range(n_folds):
+            folds = folds + [i]*fold_len
+        if len(folds) > n:
+            folds = folds[:n]
+        if len(folds) < n:
+            folds = folds + [i]*(n - len(folds))
+        assert(len(folds) == n)
+        ind = np.random.permutation(n)
+        new_data = []
+        new_labels = []
+        for i in ind:
+            new_data.append(data[i])
+            new_labels.append(labels[i])
+        data = new_data
+        labels = new_labels
+
+    cross_val_data = []
+    cross_val_labels = []
+    for f in range(n_folds):
+        left = np.where(folds == f)[0].min()
+        right = np.where(folds == f)[0].max()
+        cross_val_data.append(data[left:right + 1])
+        cross_val_labels.append(list(labels[left:right + 1]))
+
+    return cross_val_data, cross_val_labels
+
+
+def read_smiles_property_file(path, delimiter=',', keep_header=False):
+    reader = csv.reader(open(path, 'r'), delimiter=delimiter)
+    data_full = np.array(list(reader))
+    if keep_header:
+        start_position = 0
+    else:
+        start_position = 1
+    assert len(data_full) > start_position
+    smiles = data_full[start_position:, 1]
+    labels = np.array(data_full[start_position:, 2], dtype='float')
+    assert len(smiles) == len(labels)
+    return smiles, labels
