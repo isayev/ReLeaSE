ef39fe6539ff33a6b6da0747d32ad8e8f2fd5e1e
mariewelt <mariewelt@storm.olexandrisayev.com>
2018-04-03 14:19:54 -0400

    minor chane

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ stack_width = 100
 stack_depth = 100
 lr = 0.01
 gen_data = GeneratorData(training_data_path=gen_data_path, use_cuda=use_cuda)
-egfr_data = PredictorData(training_data_path=egfr_data_path)
+egfr_data = PredictorData(path=egfr_data_path)
 egfr_data.binarize(threshold=7.0)
 print('I am here')
 my_generator = StackAugmentedRNN(input_size=gen_data.n_characters, hidden_size=hidden_size,
                                  output_size=gen_data.n_characters, stack_width=stack_width,
                                  stack_depth=stack_depth, use_cuda=use_cuda, n_layers=1,
                                  optimizer='Adadelta', lr=lr)
-my_generator.load_model('Model_checkpoints/generator/policy_gradient_egfr_max')
+my_generator.load_model('/home/mariewelt/Notebooks/PyTorch/Model_checkpoints/generator/policy_gradient_egfr_max')
 
 egfr_predictor = RandomForestQSAR(n_estimators=100, n_ensemble=5)
 egfr_predictor.load_model('/home/mariewelt/Notebooks/PyTorch/data/RF/EGFR_RF')
