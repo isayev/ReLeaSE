e1df74831becd871c7546efb5159cf5cf8df7e38
mariewelt <mariewelt@storm.olexandrisayev.com>
2018-04-06 12:23:21 -0400

    Fixes to working version

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import torch
 
+import numpy as np
+
 from stackRNN import StackAugmentedRNN
 from predictor import RandomForestQSAR
 from data import GeneratorData
 from data import PredictorData
+from data import sanitize_smiles
 from reinforcement import Reinforcement
 from ReplayMemory import ReplayMemory
 
@@ -32,7 +35,8 @@ my_generator = StackAugmentedRNN(input_size=gen_data.n_characters, hidden_size=h
 if use_cuda:
     my_generator = my_generator.cuda()
 
-my_generator.load_model('/home/mariewelt/Notebooks/PyTorch/Model_checkpoints/generator/policy_gradient_egfr_max')
+#my_generator.load_model('/home/mariewelt/Notebooks/PyTorch/Model_checkpoints/generator/policy_gradient_egfr_max')
+my_generator.load_model('/home/mariewelt/Notebooks/PyTorch/Model_checkpoints/generator/checkpoint_lstm')
 
 egfr_predictor = RandomForestQSAR(n_estimators=100, n_ensemble=5)
 egfr_predictor.load_model('/home/mariewelt/Notebooks/PyTorch/data/RF/EGFR_RF')
@@ -44,18 +48,24 @@ for i in range(len(egfr_data.smiles)):
     if egfr_data.binary_labels[i] == 1.0:
         replay.push(egfr_data.smiles[i])
 
-for _ in range(10):
+generated = []
+for _ in range(replay.capacity):
+    generated.append(my_generator.evaluate(gen_data))
+
+sanitized = sanitize_smiles(generated)
+
+for sm in sanitized:
+    if sm is not None:
+        replay.push(sm)
+
+for _ in range(20):
     RL.policy_gradient_replay(gen_data, replay)
 
+f = open('generated.smi', 'w')
 for _ in range(10):
-    generated = my_generator.evaluate(gen_data, )
-    print(generated)
-
-
-
-
-
-
-
-
-
+    generated = my_generator.evaluate(gen_data)
+    if generated[-1] == '>':
+        sanitized = sanitize_smiles([generated[1:-1]])[0]
+        if sanitized is not None:
+            f.writelines(sanitized + '\n')
+f.close()
