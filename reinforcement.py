8600ffe700f3c650af0fff4fa3d5e4b8d508d5c6
Maria Popova <mariewelt@gmail.com>
2018-08-14 13:55:15 -0700

    Update reinforcement.py

--- reinforcement.py
+++ reinforcement.py
@@ -59,7 +59,7 @@ class Reinforcement(object):
         rl_loss.backward()
         self.generator.optimizer.step()
 
-        return rl_loss.data[0]
+        return rl_loss.item()
 
     def policy_gradient(self, data,  threshold, prime_str='<', end_token='>', predict_len=200, temperature=0.8, n_batch=100):
 
@@ -127,7 +127,7 @@ class Reinforcement(object):
         rl_loss.backward()
         self.generator.optimizer.step()
 
-        return rl_loss.data[0]
+        return rl_loss.item()
 
     def transfer_learning(self, data, n_epochs, augment=False):
         _ = self.generator.fit(data, n_epochs, augment=augment)
